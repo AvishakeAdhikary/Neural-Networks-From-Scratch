67a6d0f9e2fc19ea36106943323d05fe49fdee36
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-06-26 12:15:54 +0530

    Constructured GPTModel

--- GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
+++ GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
@@ -21,6 +21,10 @@ class GPTModel(torch.nn.Module):
         self.transformer = torch.nn.ModuleDict(dict(
             wordTokenEmbeddings = torch.nn.Embedding(configuration.vocabularySize, configuration.numberOfEmbeddingDimensions),
             wordPositionalEmbeddings = torch.nn.Embedding(configuration.blockSize, configuration.numberOfEmbeddingDimensions),
+            hidden = torch.nn.ModuleList(Block(configuration) for _ in range(configuration.numberOfLayers)),
+            finalLayerNorm = torch.nn.LayerNorm(configuration.numberOfEmbeddingDimensions)
         ))
+
+        self.languageModelingHead = torch.nn.Linear(configuration.numberOfEmbeddingDimensions, configuration.vocabularySize, bias=False)
 
 model = GPTModel(GPTConfiguration())

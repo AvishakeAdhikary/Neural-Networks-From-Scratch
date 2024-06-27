df067b50ee9e4b0962c79443c83411206aff3169
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-06-27 12:44:12 +0530

    Implemented transformer block

--- GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
+++ GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
@@ -2,6 +2,18 @@ from dataclasses import dataclass
 import torch
 import torch.nn.functional as F
 
+# Transformer Block
+class Block:
+    def __init__(self, configuration):
+        super().__init__()
+        self.layerNormalization1 = torch.nn.LayerNorm(configuration.numberOfEmbeddingDimensions)
+        self.attention = CausalSelfAttention(configuration)
+        self.layerNormalization2 = torch.nn.LayerNorm(configuration.numberOfEmbeddingDimensions)
+        self.multiLayerPerceptron = MultiLayerPerceptron(configuration)
+    
+    def forward(self, inputs):
+        inputs = inputs + self.attention(self.layerNormalization1(inputs))
+        inputs = inputs + self.multiLayerPerceptron(self.layerNormalization1(inputs))
 
 # GPT configuration hyper-parameters
 @dataclass

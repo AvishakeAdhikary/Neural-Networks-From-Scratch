0a8a92fac74a8c6b0880ce6bf8ab1624eff7cdd4
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2024-07-08 21:23:37 +0530

    Loaded Weights of GPT-2 Model to Custom GPT-2.5 model

--- GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
+++ GPT Scripts/GPT 2.5 Scripts/GPT_v2.5.py
@@ -9,7 +9,7 @@ class CausalSelfAttention(torch.nn.Module):
         super().__init__()
         assert configuration.numberOfEmbeddingDimensions % configuration.numberOfHeads == 0
         self.causalAttention = torch.nn.Linear(configuration.numberOfEmbeddingDimensions, 3 * configuration.numberOfEmbeddingDimensions)
-        self.causalProjection = torch.nn.Linear(3 * configuration.numberOfEmbeddingDimensions, configuration.numberOfEmbeddingDimensions)
+        self.causalProjection = torch.nn.Linear(configuration.numberOfEmbeddingDimensions, configuration.numberOfEmbeddingDimensions)
         self.numberOfHeads = configuration.numberOfHeads
         self.numberOfEmbeddingDimensions = configuration.numberOfEmbeddingDimensions
         self.register_buffer("bias", torch.tril(torch.ones(configuration.blockSize, configuration.blockSize)).view(1, 1, configuration.blockSize, configuration.blockSize))
@@ -60,11 +60,11 @@ class Block(torch.nn.Module):
 # GPT configuration hyper-parameters
 @dataclass
 class GPTConfiguration:
-    blockSize = 1024
-    vocabularySize = 50257
-    numberOfLayers = 12
-    numberOfHeads = 12
-    numberOfEmbeddingDimensions = 768
+    blockSize: int = 1024
+    vocabularySize: int = 50257
+    numberOfLayers: int = 12
+    numberOfHeads: int = 12
+    numberOfEmbeddingDimensions: int = 768
 
 # GPT model architecture
 class GPTModel(torch.nn.Module):
@@ -86,17 +86,59 @@ class GPTModel(torch.nn.Module):
     def from_pretrained(cls, modelType):
         assert modelType in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
         from transformers import GPT2LMHeadModel
-        print("Loading weights from pretrained gpt: %s" % model_type)
+        print("Loading weights from pretrained gpt: %s" % modelType)
         
         # Creating separate configurations for separate GPT-2 models
+        blockSize = 1024
+        vocabularySize = 50257
         configurationArguements = {
             'gpt2':         dict(numberOfLayers=12, numberOfHeads=12, numberOfEmbeddingDimensions=768),  # 124M parameters
             'gpt2-medium':  dict(numberOfLayers=24, numberOfHeads=16, numberOfEmbeddingDimensions=1024), # 350M parameters
             'gpt2-large':   dict(numberOfLayers=36, numberOfHeads=20, numberOfEmbeddingDimensions=1280), # 774M parameters
             'gpt2-xl':      dict(numberOfLayers=48, numberOfHeads=25, numberOfEmbeddingDimensions=1600), # 1558M parameters
         }[modelType]
-        configurationArguements['vocab_size'] = 50257
-        configurationArguements['block_size'] = 1024
+        configurationArguements['vocabularySize'] = 50257
+        configurationArguements['blockSize'] = 1024
 
-model = GPTModel(GPTConfiguration())
+        configuration = GPTConfiguration(**configurationArguements)
+        model = GPTModel(configuration)
+        stateDictionary = model.state_dict()
+        stateDictionaryKeys = stateDictionary.keys()
+        stateDictionaryKeys = [key for key in stateDictionaryKeys if not key.endswith('.attention.bias')]
+
+        huggingfaceModel = GPT2LMHeadModel.from_pretrained(modelType)
+        huggingfaceStateDictionary = huggingfaceModel.state_dict()
+        huggingfaceStateDictionaryKeys = huggingfaceStateDictionary.keys()
+        huggingfaceStateDictionaryKeys = [key for key in huggingfaceStateDictionaryKeys if not key.endswith('.attn.masked_bias')]
+        huggingfaceStateDictionaryKeys = [key for key in huggingfaceStateDictionaryKeys if not key.endswith('.attn.bias')]
+        transposedParameters = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        assert len(huggingfaceStateDictionaryKeys) == len(stateDictionaryKeys), f"Mismatched Keys: {len(huggingfaceStateDictionaryKeys)} != {len(stateDictionaryKeys)}"
+
+        parameterKeyMapping = {
+            customKey: huggingfaceKey
+            for customKey, huggingfaceKey in zip(stateDictionaryKeys, huggingfaceStateDictionaryKeys)
+            }
+
+        for customKey, huggingfaceKey in parameterKeyMapping.items():
+            if (huggingfaceStateDictionary[huggingfaceKey].shape != stateDictionary[customKey].shape):
+                # Special treatment for the Conv1D weights (Transposed Weights)
+                if (huggingfaceKey.endswith(word) for word in transposedParameters):
+                    assert huggingfaceStateDictionary[huggingfaceKey].shape[::-1] == stateDictionary[customKey].shape
+                    with torch.no_grad():
+                        stateDictionary[customKey].copy_(huggingfaceStateDictionary[huggingfaceKey].t())
+                # Vanilla copy for other parameters
+                else:
+                    assert huggingfaceStateDictionary[huggingfaceKey].shape == stateDictionary[customKey].shape
+                    with torch.no_grad():
+                        stateDictionary[customKey].copy_(huggingfaceStateDictionary[huggingfaceKey])
+        
+        return model
+
+model = GPTModel.from_pretrained('gpt2')
 print(model)
+
+
+# # If you want to remove the HuggingFace Model manually
+# from transformers import file_utils
+# print(file_utils.default_cache_path)
